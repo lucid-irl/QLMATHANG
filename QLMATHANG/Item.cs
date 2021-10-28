f1ed734ff0dacab5a5365cc5dcb8ff1e0e5bd18c
lucid-irl <tranhuyhoang3103@gmail.com>
2021-10-29 00:31:04 +0700

    init item.cs

---
+++ QLMATHANG/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLMATHANG
+{
+    class Item
+    {
+        public String MAHANG { set; get; }
+        public String TENHANG { set; get; }
+        public DateTime NGAYNHAP { set; get; }
+        public Int64 SOLUONG { set; get; }
+        public String MANCC { set; get; }
+        public Item(String MAHANG, String TENHANG, DateTime NGAYNHAP, Int64 SOLUONG, String MANCC)
+        {
+            this.MAHANG = MAHANG;
+            this.TENHANG = TENHANG;
+            this.NGAYNHAP = NGAYNHAP;
+            this.SOLUONG = SOLUONG;
+            this.MANCC = MANCC;
+        }
+    }
+}
